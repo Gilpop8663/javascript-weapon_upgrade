2985231dd6834c2a0a46bd696eef92db14f13d60
Gilpop8663 <wolfye@naver.com>
2022-12-16 04:54:20 +0900

    feat(Constant): 게임에 사용되는 에러 메세지 상수화

--- src/Constant.js
+++ src/Constant.js
@@ -27,6 +27,18 @@ const GAME_MESSAGE = Object.freeze({
   showGameResult: (grade) => `최종 강화 결과: +${grade}강`,
 });
 
+const ERROR_SUBJECT = '[ERROR]';
+
+const ERROR_MESSAGE = Object.freeze({
+  enforce: `${ERROR_SUBJECT} 강화 도전 여부는 도전: ${GAME_STRING.challenge}, 중단: ${GAME_STRING.stop} 만 입력 가능합니다.`,
+  miniGame: `${ERROR_SUBJECT} 미니 게임의 입력은 숫자: ${GAME_NUMBER.randomStart} ~ ${GAME_NUMBER.randomEnd} 사이의 수, 홀: ${GAME_STRING.odd}, 짝: ${GAME_STRING.even} 만 입력 가능합니다.`,
+  interger: `${ERROR_SUBJECT} 미니 게임의 숫자는 정수만 가능합니다.`,
+});
+
 module.exports = {
+  ERROR_SUBJECT,
+  ERROR_MESSAGE,
+  GAME_NUMBER,
   GAME_MESSAGE,
+  GAME_STRING,
 };
