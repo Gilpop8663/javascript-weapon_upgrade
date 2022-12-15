52a9044ea967dc519bb2edfed17c9a88fda46f62
Gilpop8663 <wolfye@naver.com>
2022-12-16 05:04:41 +0900

    feat(InputView): 강화 도전 여부를 입력받는 기능

--- src/controller/UpgradeGame.js
+++ src/controller/UpgradeGame.js
@@ -1,8 +1,14 @@
+const { readChallengeCommand } = require('../view/InputView');
 const { printStart } = require('../view/OutputView');
 
 class UpgradeGame {
   startGame() {
     printStart();
+    readChallengeCommand(this.checkChellengeCommand.bind(this));
+  }
+
+  checkChellengeCommand(command) {
+    console.log(command);
   }
 }
 
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,6 +1,11 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { GAME_MESSAGE } = require('../Constant');
+
 const InputView = {
   // 강화 도전 여부
-  readChallengeCommand() {},
+  readChallengeCommand(callback) {
+    Console.readLine(GAME_MESSAGE.enforce, callback);
+  },
 
   // 미니 게임 숫자 혹은 커맨드
   readMiniGameInput() {},
